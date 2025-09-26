896c19dd632362c3cc4f1973c684db1be3b6e246
ettaoussi <ettaoussiilyas0.1@gmail.com>
2025-09-26 12:55:34 +0100

    Update `ClientRepository` to include `Manager` in `addClient` method signature

--- src/repository/interfaces/ClientRepository.java
+++ src/repository/interfaces/ClientRepository.java
@@ -2,6 +2,8 @@ package repository.interfaces;
 
 import model.Account;
 import model.Client;
+import model.Manager;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -9,7 +11,7 @@ import java.util.Optional;
 
 public interface ClientRepository {
 
-    public boolean addClient(int idClient, String firstName, String lastName, String email, String password);
+    public boolean addClient(int idClient, String firstName, String lastName, String email, String password, Manager manager);
     public boolean removeClient(int idClient);
     public boolean updateClient(int idClient, String firstName, String lastName, String email, String password);
     public Optional<Client> getClientById(int idClient);
